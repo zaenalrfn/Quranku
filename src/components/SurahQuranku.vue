59da4e264a50a95007df8d47812406f23bf2999a
Zaenal Arifin <79745345+zaenalrfn@users.noreply.github.com>
2024-03-28 15:37:14 +0700

    fix surah,ayat quranku

--- src/components/SurahQuranku.vue
+++ src/components/SurahQuranku.vue
@@ -1,173 +1,33 @@
 <template>
   <div class="main mt-96">
-    <div class="surah-quranku">
+    <div class="surah-quranku" v-for="(surah, index) in dataSurah" :key="index">
       <div
         class="surah-quranku-nav w-full h-12 py-3.5 px-3.5 rounded-lg flex justify-between items-center mt-10 bg-[rgba(18,_25,_49,_0.05)]"
       >
         <div
           class="urut-surah-quranku w-5 h-5 p-3 bg-text_color_bold_1 rounded-full flex justify-center items-center"
         >
-          <h2 class="text-white">1</h2>
+          <h2 class="text-white">{{ surah.ayah }}</h2>
         </div>
         <div class="surah-quranku-play-simpan flex gap-6">
-          <h2 class="text-2xl"><i class="bi bi-play"></i></h2>
+          <h2 class="text-2xl" @click="toggleAudio">
+            <i class="bi bi-play"></i>
+          </h2>
+          <audio
+            class="audioSurah"
+            ref="audioSurah"
+            @ended="audioEnded"
+            :src="surah.audio"
+          ></audio>
           <h2 class="text-xl"><i class="bi bi-bookmark"></i></h2>
         </div>
       </div>
       <div class="surah-quranku-arab-arti">
         <h2 class="mb-4 text-right mt-3 text-4xl text-text_color_bold_1">
-          بِسْمِ اللّٰهِ الرَّحْمٰنِ الرَّحِيْمِ
+          {{ surah.arab }}
         </h2>
         <p class="text-text_color_bold_1 text-[16px] mb-4">
-          Dengan menyebut nama Allah Yang Maha Pengasih lagi maha penyayang.
-        </p>
-        <hr />
-      </div>
-    </div>
-    <div class="surah-quranku">
-      <div
-        class="surah-quranku-nav w-full h-12 py-3.5 px-3.5 rounded-lg flex justify-between items-center mt-10 bg-[rgba(18,_25,_49,_0.05)]"
-      >
-        <div
-          class="urut-surah-quranku w-5 h-5 p-3 bg-text_color_bold_1 rounded-full flex justify-center items-center"
-        >
-          <h2 class="text-white">2</h2>
-        </div>
-        <div class="surah-quranku-play-simpan flex gap-6">
-          <h2 class="text-2xl"><i class="bi bi-play"></i></h2>
-          <h2 class="text-xl"><i class="bi bi-bookmark"></i></h2>
-        </div>
-      </div>
-      <div class="surah-quranku-arab-arti">
-        <h2 class="mb-4 text-right mt-3 text-4xl text-text_color_bold_1">
-          اَلْحَمْدُ لِلّٰهِ رَبِّ الْعٰلَمِيْنَۙ
-        </h2>
-        <p class="text-text_color_bold_1 text-[16px] mb-4">
-          Segala puji bagi Allah, Tuhan seluruh alam.
-        </p>
-        <hr />
-      </div>
-    </div>
-    <div class="surah-quranku">
-      <div
-        class="surah-quranku-nav w-full h-12 py-3.5 px-3.5 rounded-lg flex justify-between items-center mt-10 bg-[rgba(18,_25,_49,_0.05)]"
-      >
-        <div
-          class="urut-surah-quranku w-5 h-5 p-3 bg-text_color_bold_1 rounded-full flex justify-center items-center"
-        >
-          <h2 class="text-white">3</h2>
-        </div>
-        <div class="surah-quranku-play-simpan flex gap-6">
-          <h2 class="text-2xl"><i class="bi bi-play"></i></h2>
-          <h2 class="text-xl"><i class="bi bi-bookmark"></i></h2>
-        </div>
-      </div>
-      <div class="surah-quranku-arab-arti">
-        <h2 class="mb-4 text-right mt-3 text-4xl text-text_color_bold_1">
-          الرَّحْمٰنِ الرَّحِيْمِۙ
-        </h2>
-        <p class="text-text_color_bold_1 text-[16px] mb-4">
-          Yang Maha Pengasih, Maha Penyayang.
-        </p>
-        <hr />
-      </div>
-    </div>
-    <div class="surah-quranku">
-      <div
-        class="surah-quranku-nav w-full h-12 py-3.5 px-3.5 rounded-lg flex justify-between items-center mt-10 bg-[rgba(18,_25,_49,_0.05)]"
-      >
-        <div
-          class="urut-surah-quranku w-5 h-5 p-3 bg-text_color_bold_1 rounded-full flex justify-center items-center"
-        >
-          <h2 class="text-white">4</h2>
-        </div>
-        <div class="surah-quranku-play-simpan flex gap-6">
-          <h2 class="text-2xl"><i class="bi bi-play"></i></h2>
-          <h2 class="text-xl"><i class="bi bi-bookmark"></i></h2>
-        </div>
-      </div>
-      <div class="surah-quranku-arab-arti">
-        <h2 class="mb-4 text-right mt-3 text-4xl text-text_color_bold_1">
-          مٰلِكِ يَوْمِ الدِّيْنِۗ
-        </h2>
-        <p class="text-text_color_bold_1 text-[16px] mb-4">
-          Pemilik hari pembalasan.
-        </p>
-        <hr />
-      </div>
-    </div>
-    <div class="surah-quranku">
-      <div
-        class="surah-quranku-nav w-full h-12 py-3.5 px-3.5 rounded-lg flex justify-between items-center mt-10 bg-[rgba(18,_25,_49,_0.05)]"
-      >
-        <div
-          class="urut-surah-quranku w-5 h-5 p-3 bg-text_color_bold_1 rounded-full flex justify-center items-center"
-        >
-          <h2 class="text-white">5</h2>
-        </div>
-        <div class="surah-quranku-play-simpan flex gap-6">
-          <h2 class="text-2xl"><i class="bi bi-play"></i></h2>
-          <h2 class="text-xl"><i class="bi bi-bookmark"></i></h2>
-        </div>
-      </div>
-      <div class="surah-quranku-arab-arti">
-        <h2 class="mb-4 text-right mt-3 text-4xl text-text_color_bold_1">
-          اِيَّاكَ نَعْبُدُ وَاِيَّاكَ نَسْتَعِيْنُۗ
-        </h2>
-        <p class="text-text_color_bold_1 text-[16px] mb-4">
-          Hanya kepada Engkaulah kami menyembah dan hanya kepada Engkaulah kami
-          mohon pertolongan.
-        </p>
-        <hr />
-      </div>
-    </div>
-    <div class="surah-quranku">
-      <div
-        class="surah-quranku-nav w-full h-12 py-3.5 px-3.5 rounded-lg flex justify-between items-center mt-10 bg-[rgba(18,_25,_49,_0.05)]"
-      >
-        <div
-          class="urut-surah-quranku w-5 h-5 p-3 bg-text_color_bold_1 rounded-full flex justify-center items-center"
-        >
-          <h2 class="text-white">6</h2>
-        </div>
-        <div class="surah-quranku-play-simpan flex gap-6">
-          <h2 class="text-2xl"><i class="bi bi-play"></i></h2>
-          <h2 class="text-xl"><i class="bi bi-bookmark"></i></h2>
-        </div>
-      </div>
-      <div class="surah-quranku-arab-arti">
-        <h2 class="mb-4 text-right mt-3 text-4xl text-text_color_bold_1">
-          اِهْدِنَا الصِّرَاطَ الْمُسْتَقِيْمَۙ
-        </h2>
-        <p class="text-text_color_bold_1 text-[16px] mb-4">
-          Tunjukilah kami jalan yang lurus.
-        </p>
-        <hr />
-      </div>
-    </div>
-    <div class="surah-quranku">
-      <div
-        class="surah-quranku-nav w-full h-12 py-3.5 px-3.5 rounded-lg flex justify-between items-center mt-10 bg-[rgba(18,_25,_49,_0.05)]"
-      >
-        <div
-          class="urut-surah-quranku w-5 h-5 p-3 bg-text_color_bold_1 rounded-full flex justify-center items-center"
-        >
-          <h2 class="text-white">7</h2>
-        </div>
-        <div class="surah-quranku-play-simpan flex gap-6">
-          <h2 class="text-2xl"><i class="bi bi-play"></i></h2>
-          <h2 class="text-xl"><i class="bi bi-bookmark"></i></h2>
-        </div>
-      </div>
-      <div class="surah-quranku-arab-arti">
-        <h2 class="mb-4 text-right mt-3 text-4xl text-text_color_bold_1">
-          صِرَاطَ الَّذِيْنَ اَنْعَمْتَ عَلَيْهِمْ ەۙ غَيْرِ الْمَغْضُوْبِ
-          عَلَيْهِمْ وَلَا الضَّاۤلِّيْنَ ࣖ
-        </h2>
-        <p class="text-text_color_bold_1 text-[16px] mb-4">
-          (yaitu) jalan orang-orang yang telah Engkau beri nikmat kepadanya;
-          bukan (jalan) mereka yang dimurkai, dan bukan (pula jalan) mereka yang
-          sesat.
+          {{ surah.text }}
         </p>
         <hr />
       </div>
@@ -177,5 +37,11 @@
 <script>
 export default {
   name: "SurahQuranku",
+  props: {
+    dataSurah: {
+      type: Object,
+      required: true,
+    },
+  },
 };
 </script>
